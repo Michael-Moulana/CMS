5c3289e65d819a3afd35b548be9cf77ad83b6f67
Michael Moulana <michaelmoulana@gmail.com>
2025-09-26 04:03:38 +1000

    CMS-126 Subtask 1: Design database schema/model for products and media.

--- backend/models/Media.js
+++ backend/models/Media.js
@@ -5,7 +5,7 @@ const MediaSchema = new mongoose.Schema({
   originalName: { type: String, required: true }, // original upload name
   mimeType: { type: String, required: true },
   size: { type: Number, required: true },
-  title: { type: String, default: "" },
+  title: { type: String, default: "" }, // title for the media, can be used for alt text
   path: { type: String, required: true }, // absolute path on server
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   createdAt: { type: Date, default: Date.now },
